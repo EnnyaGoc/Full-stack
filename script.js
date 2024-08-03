6c1ef159958ad8ea77ad7dca4eaf155b0eba366e
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-08-03 19:41:00 -0300

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+const APILINK = ''
+const IMG_PATH = ''
+const SEARCHAPI = ''
+
+
+const main = document.getElementById("section");
+const form = document.getElementById("form");
+const search = document.getElementById("query");
+
+function returnMovies(url){
+    
+}
