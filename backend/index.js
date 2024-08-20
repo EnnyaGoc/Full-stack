3b4b711cc24df05ae060077ec0742bdb09a6f645
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-08-19 23:40:46 -0300

    .

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,30 @@
 //npm init para criar o package.json
 
-
 import app from "./server.js"
 import mongodb from "mongodb"
-import ReviewsDAO from "./dao/reviewsDAO.js"
+//import ReviewsDAO from "./dao/reviewsDAO.js"
+
+const MongoClient = mongodb.MongoClient
+//const mongo_username = process.env['MONGO_USERNAME']
+//const mongo_password = process.env['MONGO_PASSWORD']
+const uri = "mongodb+srv://ennya:Princesa4@cluster0.rzvea.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const port = 8000
+
+MongoClient.connect(
+    uri,
+    {
+        maxPoolSize: 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true
+    }
+)
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
+    })
+    .then(async client => {
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+        })
+    })
