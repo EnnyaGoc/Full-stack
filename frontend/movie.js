4e5036018cef026c0628fd9c4b93dc623051488b
Ênnya Gomes <ennyagomesoc@gmail.com>
2024-10-10 22:46:18 -0300

    Update movie.js

--- frontend/movie.js
+++ frontend/movie.js
@@ -12,7 +12,31 @@ const title = document.getElementById("title");
 title.innerText = movieTitle;
 
 
+
+const div_new = document.createElement('div');
+div_new.innerHTML = `
+
+                    <div class="row">
+                        <div class="column">
+                            <div class="card">
+                            New Review
+                                <p><strong>Review: </strong>
+                                <input type="text" id="new_review" value="">
+                                </p>
+                                <p><strong>User: </strong>
+                                <input type="text" id="new_user" value="">
+                                </p>
+                                    <p><a href="#" onclick="saveReview('new_review', 'new_user')">salvar</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    `
+
+main.appendChild(div_card);
+    
 returnReviews(APILINK);
+
 
 function returnReviews(url){
     fetch(url + "movie/" + movieId).then(res => res.json()) // Faz uma requisição HTTP para a URL fornecida. Quando a resposta for recebida, ela será convertida para JSON.
@@ -63,7 +87,7 @@ function saveReview(reviewInputId, userInputId, id="") {
     const review = document.getElementById(reviewInputId).value;
     const user = document.getElementById(userInputId).value;
 
-    id (id){
+    if (id){
         fetch(APILINK + id, {
             method: 'PUT',
             headers: {
@@ -77,6 +101,31 @@ function saveReview(reviewInputId, userInputId, id="") {
                 location.reload();
             });
     }
+    else {
+        fetch(APILINK + id, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"user": user, "review": review, "movieId": movieId})
+        }).then(res => res.json())
+            .then(res => {
+                console.log(res)
+                location.reload();
+            });
+    }
+}
+
+function deleteReview(id) {
+    fetch(APILINK + id, {
+        method: 'DELETE'
+    }).then(res => res.json())
+    .then(res => {
+        console.log(res)
+        location.reload();
+    });
+    
 }
 
 
